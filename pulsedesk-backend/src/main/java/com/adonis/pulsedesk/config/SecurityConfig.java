1421c68dc8de282ce4811299c5da0340cd1bdae6
Yash Kumar <yashkumaryh06@gmail.com>
2026-03-30 01:46:00 +0300

    Update SecurityConfig.java

--- pulsedesk-backend/src/main/java/com/adonis/pulsedesk/config/SecurityConfig.java
+++ pulsedesk-backend/src/main/java/com/adonis/pulsedesk/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                         .requestMatchers("/", "/index.html", "/Adonisu.png").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin();
+                .httpBasic();
 
         return http.build();
     }
